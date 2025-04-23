6c580baaf5c7d2fb46c44b5bd7e68fd6ecb6ec0c
Manulaz <manuel7super@gmail.com>
2025-04-23 18:15:36 -0300

    2

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from machine import Pin, PWM, RTC
 import time
+import network
+import ntptime
+
+def conectar_wifi(ssid, senha):
+    wifi = network.WLAN(network.STA_IF)
+    wifi.active(True)
+    if not wifi.isconnected():
+        print("Conectando ao Wi-Fi")
+        wifi.connect(ssid, senha)
+        while not wifi.isconnected():
+            pass
+    print('Conectado! IP:', wifi.ifconfig()[0])
 
 #pino = pino usado pro servo
 class servo:
@@ -8,7 +20,7 @@ class servo:
 
     def set_angle(self, angle):
         # Converte o ângulo para o valor de duty cycle, se ai depende do angle que a gente quer
-        duty = int(((angle / 180) * 75) + 25)
+        duty = int((angle / 180) * 75) + 40
         self.pwm.duty(duty)
 
     def acionar(self):
@@ -21,10 +33,7 @@ class servo:
 class Timing:
     def __init__(self):
         self.rtc = RTC()
-        self.rtc.datetime((2023, 10, 1, 0, 0, 0, 0, 0)) # Define a data e hora inicial
 
-    def set_time(self, year, month, day, hour, minute, second):
-        self.rtc.datetime((year, month, day, 0, hour, minute, second, 0))
-
-    def get_time(self):
-        return self.rtc.datetime()
+        try:
+            ntptime.settime()
+            
